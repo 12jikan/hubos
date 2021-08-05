9158537d98224d3c7d66e13c5edecba2fdf5a852
Jareem <Jareem.X.Hoff.-ND@abc.com>
2021-08-04 23:25:48 -0400

    nav push

---
+++ components/NavbarComponent.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function NavbarComponent(): JSX.Element {
+    return (
+        <>
+            <div className="nav-container">
+                <div>
+                    <div></div>
+                </div>
+            </div>
+
+            <style jsx>{`
+                .nav-container {
+                    display: grid;
+                }
+            `}</style>
+        </>
+    );
+}
